32fd80562c638c5150528226a729eae32ecd5e2c
Harris Pyo <HPW9229@gmail.com>
2023-10-23 10:46:52 -0400

    Updated tailwind.config file to include custom branding colors to use indead of hex codes.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,16 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'midnight-sky' : '#172D42',
+        'aero' : '#0E2D64',
+        'hydrogen': '#12A1E0',
+        'clear': '#F9F8F5',
+        'dust': '#646668',
+        'slate': '#808D87'
+      }
+    },
   },
   plugins: [],
 } satisfies Config;
