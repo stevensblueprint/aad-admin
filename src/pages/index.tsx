82856c2010d3c406a371f3e296282c949b695f28
Eric Zhu <eric@ericz.me>
2023-11-05 22:40:54 -0500

    chore(prettier)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,23 +10,22 @@ export default function Home() {
   return (
     <>
       <Head>
-       <title>
-        Asian American Dream Admin
-       </title>
+        <title>Asian American Dream Admin</title>
       </Head>
       <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-midnight-sky to-aero">
-        <h1 className="mt-12 text-[#F9F8F5] text-5xl font-bold">
+        <h1 className="mt-12 text-5xl font-bold text-[#F9F8F5]">
           Asian American Dream Admin
         </h1>
-        <div className="mt-20 mx-14 flex flex-col flex-grow justify-between items-center">
-          <p className="text-[#F9F8F5] text-base w-4/5">
-            &emsp; Founded in May 2021, our mission is to provide mentorship networks,
-            professional development training, and career advancement opportunities for
-            underserved Pan Asian American undergraduates, with the goal of helping them
-            achieve their unique vision of the Asian American dream. Since our inception,
-            we’ve had 752 unique students register for our programming. However, with more
-            than 1 million Pan Asian American undergraduates (and growing), there is work to
-            be done.
+        <div className="mx-14 mt-20 flex flex-grow flex-col items-center justify-between">
+          <p className="w-4/5 text-base text-[#F9F8F5]">
+            &emsp; Founded in May 2021, our mission is to provide mentorship
+            networks, professional development training, and career advancement
+            opportunities for underserved Pan Asian American undergraduates,
+            with the goal of helping them achieve their unique vision of the
+            Asian American dream. Since our inception, we’ve had 752 unique
+            students register for our programming. However, with more than 1
+            million Pan Asian American undergraduates (and growing), there is
+            work to be done.
           </p>
           <AuthShowcase />
         </div>
