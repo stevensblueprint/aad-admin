383d727fc9ba8f2e4f0899517d0deb90fc23ed97
nad <58147198+nad2040@users.noreply.github.com>
2023-11-06 16:42:21 -0500

    fix(tRPC): fixed router setup, getAll and getById work

---
+++ src/server/api/routers/dbquery.ts
@@ -0,0 +1,33 @@
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
+
+import { db } from "~/server/db";
+
+export const dbQueryRouter = createTRPCRouter({
+  getById: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      try {
+        return await db.user.findUniqueOrThrow({
+          where: {
+            id: input.id,
+          },
+        });
+      } catch (error) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: '"{input.id}" not found in database',
+        });
+      }
+    }),
+
+  getAll: protectedProcedure.query(async () => {
+    return await db.user.findMany();
+  }),
+});
