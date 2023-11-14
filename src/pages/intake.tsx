ddf9253cf2f884906f3841b8b2852633471bcf21
Christian Apostol <christiand.apostol@outlook.com>
2023-11-14 15:33:05 -0500

    default json form example

--- src/pages/intake.tsx
+++ src/pages/intake.tsx
@@ -1,8 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import React, { useState } from "react";
+import { JsonForms } from "@jsonforms/react";
+import {
+  materialRenderers,
+  materialCells,
+} from "@jsonforms/material-renderers";
+import { person } from "@jsonforms/examples";
+
 // https://jsonforms.io/
 // current AAD Form: https://forms.office.com/pages/responsepage.aspx?id=t5piMQcNikilfZtYTEzdYHjzwie7DY1CmFODRRLdrqlURUtNWThaMElZTVJIOTU4OUhYV0g5NVNGMyQlQCN0PWcu
 
 // TODO: Place behind Auth http://localhost:3000/intake
 
+const schema = person.schema;
+const uischema = person.uischema;
+const initialData = person.data;
+
 export default function Intake() {
-  return <div>This is the intake form</div>
+  const [data, setData] = useState(initialData);
+
+  return (
+    <div className="App">
+      <JsonForms
+        schema={schema}
+        uischema={uischema}
+        data={data}
+        renderers={materialRenderers}
+        cells={materialCells}
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        onChange={({ data, errors }) => setData(data)}
+      />
+    </div>
+  );
 }
