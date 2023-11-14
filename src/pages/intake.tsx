371600750ed9165e4abe8b62019221f6a74a581d
Christian Apostol <christiand.apostol@outlook.com>
2023-11-14 15:10:00 -0500

    added jsonforms dependencies

---
+++ src/pages/intake.tsx
@@ -0,0 +1,8 @@
+// https://jsonforms.io/
+// current AAD Form: https://forms.office.com/pages/responsepage.aspx?id=t5piMQcNikilfZtYTEzdYHjzwie7DY1CmFODRRLdrqlURUtNWThaMElZTVJIOTU4OUhYV0g5NVNGMyQlQCN0PWcu
+
+// TODO: Place behind Auth http://localhost:3000/intake
+
+export default function Intake() {
+  return <div>This is the intake form</div>
+}
