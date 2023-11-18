47f6427d38ade34afc0054dfb3a5b0c5ee6603f8
Caposto <christiand.apostol@outlook.com>
2023-11-18 16:27:18 -0500

    Created  generic form component

---
+++ src/pages/forms/Form.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { JsonForms } from "@jsonforms/react";
+import { materialRenderers } from "@jsonforms/material-renderers";
+import { type UISchemaElement } from "@jsonforms/core";
+
+interface FormProps {
+  schema: object;
+  uischema: UISchemaElement; // FIXME: IDK about this
+  initialData: unknown;
+}
+
+// Generic Form Component that can be customized off of JSON schemas
+export default function Form({ schema, uischema, initialData }: FormProps) {
+  const [formData, setFormData] = useState(initialData);
+
+  const onChange = ({ data }: { data: unknown }) => setFormData(data); // Specifying the type of data
+
+  return (
+    <JsonForms
+      schema={schema}
+      uischema={uischema}
+      data={formData}
+      renderers={materialRenderers}
+      onChange={onChange}
+    />
+  );
+}
--- src/pages/intake.tsx
+++ src/pages/intake.tsx
@@ -1,35 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import React, { useState } from "react";
-import { JsonForms } from "@jsonforms/react";
-import {
-  materialRenderers,
-  materialCells,
-} from "@jsonforms/material-renderers";
+import Form from "./forms/Form";
 import { person } from "@jsonforms/examples";
 
-// https://jsonforms.io/
-// current AAD Form: https://forms.office.com/pages/responsepage.aspx?id=t5piMQcNikilfZtYTEzdYHjzwie7DY1CmFODRRLdrqlURUtNWThaMElZTVJIOTU4OUhYV0g5NVNGMyQlQCN0PWcu
-
-// TODO: Place behind Auth http://localhost:3000/intake
-
-const schema = person.schema;
-const uischema = person.uischema;
-const initialData = person.data;
-
 export default function Intake() {
-  const [data, setData] = useState(initialData);
-
   return (
     <div className="App">
-      <JsonForms
-        schema={schema}
-        uischema={uischema}
-        data={data}
-        renderers={materialRenderers}
-        cells={materialCells}
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        onChange={({ data, errors }) => setData(data)}
+      <Form
+        schema={person.schema}
+        uischema={person.uischema}
+        initialData={person.data}
       />
     </div>
   );
