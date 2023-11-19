d4a46bc48cca8eeecb55aabe9fce941563322904
Caposto <christiand.apostol@outlook.com>
2023-11-19 15:01:10 -0500

    arranged folders to resolve build failure

--- src/pages/intake.tsx
+++ src/pages/intake.tsx
@@ -1,7 +1,7 @@
-import Form from "./forms/Form";
-import menteeIntakeSchema from "src/pages/forms/MenteeIntake/schema.json";
-import menteeIntakeUiSchema from "src/pages/forms/MenteeIntake/uischema.json";
-import menteeIntakeData from "src/pages/forms/MenteeIntake/data.json";
+import Form from "../components/forms/Form";
+import menteeIntakeSchema from "~/components/forms/MenteeIntake/schema.json";
+import menteeIntakeUiSchema from "~/components/forms/MenteeIntake/uischema.json";
+import menteeIntakeData from "~/components/forms/MenteeIntake/data.json";
 
 export default function Intake() {
   return (
