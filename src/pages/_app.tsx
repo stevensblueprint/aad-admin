219f3570abae01bf202c8faf78cfa4d3c8a37720
Richard Kirk <92767187+ulrokx@users.noreply.github.com>
2023-11-28 09:16:05 -0500

    Feature/install primereact (#62)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { PrimeReactProvider } from "primereact/api";
+import designSystem from "../styles/designSystem";
+// import Tailwind from "primereact/passthrough/tailwind"; // replace 'designSystem' in provider with 'Tailwind' for default PrimeReact styling
 import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
@@ -12,7 +15,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <PrimeReactProvider value={{ unstyled: true, pt: designSystem }}>
+        <Component {...pageProps} />
+      </PrimeReactProvider>
     </SessionProvider>
   );
 };
