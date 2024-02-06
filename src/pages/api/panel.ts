2803d3a68ca458da14ebd6cf5ddba89f63fcdcc4
Richard Kirk <richard.kirk790@gmail.com>
2024-02-06 14:32:41 -0500

    Add tRPC panel for debugging

---
+++ src/pages/api/panel.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { renderTrpcPanel } from "trpc-panel";
+import { appRouter } from "../../server/api/root";
+
+export default function handler(_: NextApiRequest, res: NextApiResponse) {
+  res.status(200).send(
+    renderTrpcPanel(appRouter, {
+      url: "http://localhost:3000/api/trpc",
+      transformer: "superjson",
+    })
+  );
+}
