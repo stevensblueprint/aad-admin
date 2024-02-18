9a0389da309928ec897732dad39197a89158e2b1
nad <58147198+nad2040@users.noreply.github.com>
2024-02-18 15:49:22 -0500

    include profile in query, lowercase profile in prisma schema

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -1,12 +1,17 @@
 import { api } from "../../utils/api";
 import { useRouter } from "next/router";
 
-export default function Profile() {
+export default function ProfilePage() {
   const router = useRouter();
   const { data, error, isLoading } = api.user.getById.useQuery({
     id: router.query.id as string,
   });
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {JSON.stringify(error)}</div>;
-  return <p>id: {data.id}, name: {data.name}, email: {data.email}, imageurl: {data.image}</p>
-};
+  return (
+    <p>
+      id: {data.id}, name: {data.name}, email: {data.email}, imageurl:{" "}
+      {data.image}
+    </p>
+  );
+}
