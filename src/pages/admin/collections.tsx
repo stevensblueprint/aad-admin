e691f2ecaa087548452b5842d31ae286b00f0155
Richard Kirk <richard.kirk790@gmail.com>
2024-02-20 15:02:08 -0500

    Added create collection form
    This commit adds a basic create collection form that needs styling and error
    handling logic.

--- src/pages/admin/collections.tsx
+++ src/pages/admin/collections.tsx
@@ -1,4 +1,5 @@
 import {
+  Container,
   Table,
   TableBody,
   TableCell,
@@ -7,34 +8,51 @@ import {
   TableRow,
 } from "@mui/material";
 import { api } from "../../utils/api";
+import { type SubmitHandler } from "react-hook-form";
+import CollectionForm, {
+  CreateCollectionData,
+} from "../../components/admin/CollectionForm";
 
 const Collections = () => {
   const { data, error, isLoading } = api.collection.getCollections.useQuery();
+  // TODO: error handling decision
+  const {
+    mutateAsync,
+    error: createError,
+    isLoading: createLoading,
+  } = api.collection.createCollection.useMutation();
+  const onSubmit: SubmitHandler<CreateCollectionData> = async (data) => {
+    debugger;
+    await mutateAsync(data);
+  };
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
   return (
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Collection Name</TableCell>
-            <TableCell>Form Name</TableCell>
-            <TableCell>Public</TableCell>
-            <TableCell>Open</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data?.map((collection) => (
-            <TableRow key={collection.id}>
-              <TableCell>{collection.name}</TableCell>
-              <TableCell>{collection.formName}</TableCell>
-              <TableCell>{collection.isPublic ? "Yes" : "No"}</TableCell>
-              <TableCell>{collection.isOpen ? "Yes" : "No"}</TableCell>
+    <Container>
+      <CollectionForm onSubmit={onSubmit} />
+      <TableContainer>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Collection Name</TableCell>
+              <TableCell>Form Name</TableCell>
+              <TableCell>Public</TableCell>
+              <TableCell>Open</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {data?.map((collection) => (
+              <TableRow key={collection.id}>
+                <TableCell>{collection.name}</TableCell>
+                <TableCell>{collection.formName}</TableCell>
+                <TableCell>{collection.isPublic ? "Yes" : "No"}</TableCell>
+                <TableCell>{collection.isOpen ? "Yes" : "No"}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
   );
 };
 
