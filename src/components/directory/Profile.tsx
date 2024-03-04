5ee76dd42d9404fc7ff95acdf89544e414b97f9f
miguel-merlin <mmerlin@stevens.edu>
2024-03-04 17:11:03 -0500

    feat: Added ProfileCardProps interface

--- src/components/directory/Profile.tsx
+++ src/components/directory/Profile.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import { Avatar } from "@mui/material";
 import { type Profile, type Prisma } from "@prisma/client";
 
-export type profileProps = Prisma.ProfileGetPayload<{
-  include: {
-    user: true;
-  };
-}>;
+interface ProfileCardProps {
+  profile: Prisma.ProfileGetPayload<{
+    include: {
+      user: true;
+    };
+  }>;
+}
 
-const Profile = ({ data }: { data: profileProps }) => {
+const Profile = ({ profile }: ProfileCardProps) => {
   return (
     <div className="flex w-[600px] gap-4 rounded-lg bg-aero p-4">
-      <Avatar src={data.user.image} sx={{ width: 80, height: 80 }} />
+      <Avatar src={profile.user.image} sx={{ width: 80, height: 80 }} />
       <div className="flex-1 text-white">
-        <p>{data.user.name}</p>
-        <p>{data.user.email}</p>
-        {data.bio && <p>data.bio</p>}
+        <p>{profile.user.name}</p>
+        <p>{profile.user.email}</p>
+        {profile.bio && <p>{profile.bio}</p>}
       </div>
     </div>
   );
