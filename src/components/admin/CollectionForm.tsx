003b4ae11df34ae3ecbdca3a0c6ec9a016c30b5c
Richard Kirk <92767187+ulrokx@users.noreply.github.com>
2024-03-08 14:28:54 -0500

    Feature/admin collections (#138)
    * Basic collections table
    
    * Added create collection form
    
    This commit adds a basic create collection form that needs styling and error
    handling logic.

---
+++ src/components/admin/CollectionForm.tsx
@@ -0,0 +1,93 @@
+import {
+  Box,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+  Select,
+  MenuItem,
+} from "@mui/material";
+import { Controller, useForm } from "react-hook-form";
+import { api } from "../../utils/api";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+export const createCollectionSchema = z.object({
+  formName: z.string(),
+  // TODO: this should change to visibility, and be an enum
+  // perhaps not visibility, but audience as all forms will require auth
+  isPublic: z.boolean(),
+  isOpen: z.boolean(),
+  name: z.string(),
+});
+
+export type CreateCollectionData = z.infer<typeof createCollectionSchema>;
+
+interface CollectionFormProps {
+  onSubmit: (data: CreateCollectionData) => void;
+}
+
+const CollectionForm = ({ onSubmit }: CollectionFormProps) => {
+  const { data: forms, error, isLoading } = api.form.getForms.useQuery({});
+  const { handleSubmit, control } = useForm<CreateCollectionData>({
+    resolver: zodResolver(createCollectionSchema),
+    defaultValues: {
+      formName: "",
+      isPublic: false,
+      isOpen: false,
+      name: "",
+    },
+  });
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <Box
+      component="form"
+      onSubmit={(...args) => void handleSubmit(onSubmit)(...args)}
+    >
+      <Controller
+        control={control}
+        render={({ field, fieldState: { error } }) => (
+          <TextField
+            label="Collection Name"
+            {...field}
+            error={!!error}
+            helperText={error?.message}
+          />
+        )}
+        name="name"
+      />
+      <Controller
+        control={control}
+        render={({ field, fieldState: { error } }) => (
+          <Select {...field} fullWidth>
+            {forms?.map(({ name }) => (
+              <MenuItem key={name} value={name}>
+                {name}
+              </MenuItem>
+            ))}
+          </Select>
+        )}
+        name="formName"
+      />
+      <Controller
+        control={control}
+        render={({ field }) => (
+          <FormControlLabel control={<Checkbox />} label="Public" {...field} />
+        )}
+        name="isPublic"
+      />
+      <Controller
+        control={control}
+        render={({ field }) => (
+          <FormControlLabel control={<Checkbox />} label="Open" {...field} />
+        )}
+        name="isOpen"
+      />
+      <Button type="submit">Create</Button>
+    </Box>
+  );
+};
+
+export default CollectionForm;
