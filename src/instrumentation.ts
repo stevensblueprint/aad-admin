c9aa1140836f0de992b89401366ab0e37b8fa966
Ezri Zhu <me@ezrizhu.com>
2024-03-15 18:16:07 -0400

    make db conn check verbose

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -3,6 +3,7 @@ import { db } from "./server/db";
 export const register = async () => {
   try {
     await db.$connect();
+    console.log("Database connection successful!");
   } catch (e) {
     console.error(e);
     process.exit(1);
