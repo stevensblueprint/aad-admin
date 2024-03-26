3b8bfe0ee5b3d0a1e3fa559fff32edcfeef361f9
miguel-merlin <mmerlin@stevens.edu>
2024-03-26 01:47:33 -0400

    fix: Fixed format errors

--- src/pages/directory.tsx
+++ src/pages/directory.tsx
@@ -2,8 +2,7 @@ import { api } from "../utils/api";
 import { ToggleButton, ToggleButtonGroup } from "@mui/material";
 import { useState, type ReactElement } from "react";
 import AdminLayout from "../components/layouts/AdminLayout";
-import { DataGrid, type GridColDef } from '@mui/x-data-grid';
-
+import { DataGrid, type GridColDef } from "@mui/x-data-grid";
 
 enum UserRole {
   MENTEE = "Mentee",
@@ -19,35 +18,37 @@ const Directory = () => {
   });
 
   const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 90 },
+    { field: "id", headerName: "ID", width: 90 },
     {
-      field: 'name',
-      headerName: 'Name',
+      field: "name",
+      headerName: "Name",
       width: 150,
       editable: false,
     },
     {
-      field: 'email',
-      headerName: 'Email',
+      field: "email",
+      headerName: "Email",
       width: 150,
       editable: false,
     },
     {
-      field: 'role',
-      headerName: 'Role',
+      field: "role",
+      headerName: "Role",
       width: 110,
       editable: false,
-      type: 'singleSelect',
+      type: "singleSelect",
       valueOptions: Object.values(UserRole),
     },
   ];
-  
-  const rows = data ? data.map(user => ({
-    id: user.id,
-    name: user.user.name,
-    email: user.user.email,
-    role: user.user.roleName,
-  })) : [];
+
+  const rows = data
+    ? data.map((user) => ({
+        id: user.id,
+        name: user.user.name,
+        email: user.user.email,
+        role: user.user.roleName,
+      }))
+    : [];
 
   return (
     <main className="flex min-h-screen w-full flex-col items-center bg-clear">
@@ -59,8 +60,7 @@ const Directory = () => {
         exclusive
         value={currentRole}
         onChange={(event, newRole: string | null) => {
-          if (newRole !== null)
-            setCurrentRole(newRole as UserRole);
+          if (newRole !== null) setCurrentRole(newRole as UserRole);
         }}
       >
         {Object.values(UserRole).map((role) => (
@@ -73,7 +73,7 @@ const Directory = () => {
           </ToggleButton>
         ))}
       </ToggleButtonGroup>
-      <div style={{ height: 400, width: '100%' }}>
+      <div style={{ height: 400, width: "100%" }}>
         <DataGrid
           rows={rows}
           columns={columns}
@@ -83,7 +83,6 @@ const Directory = () => {
       </div>
     </main>
   );
-
 };
 
 Directory.getLayout = function getLayout(page: ReactElement) {
