ff4c48916205f5ab7f26e148cd372270ce0f1b9d
Harris Pyo <HPW9229@gmail.com>
2024-03-25 23:19:19 -0400

    Created router mutation for edit profile backend form validation and updating values in database. Started working on adding initial value to display when editing profile and edited schema to include bio, college, and industries.

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -46,4 +46,63 @@ export const userRouter = createTRPCRouter({
         },
       });
     }),
+
+  updateProfile: protectedProcedure
+    .input(z.object({
+      name: z.string().min(1),
+      emailAddress: z.string().email(),
+      dateOfBirth: z.string().min(1),
+      biography: z.string().min(1),
+      selectedUniversity: z.string().min(1),
+      topIndustries: z
+        .array(z.string())
+        .min(1),
+    }).partial())
+    .mutation(async ({ input, ctx }) => {
+      //Check if the user exits before trying to modify their information
+      let profileExits = db.user.findUnique({
+        where: {
+          id: ctx.session.user.id,
+        },
+      });
+      if (!profileExits) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: '"{input.name}" not found in database',
+        });
+      }
+      //Update the user's information
+      try {
+        return await db.user.update({
+          where: {
+            id: ctx.session.user.id,
+          },
+          data: {
+            name: input.name,
+            email: input.emailAddress,
+            profile: {
+              update: {
+                where: {
+                  userId: ctx.session.user.id,
+                },
+                data: {
+                  dateOfBirth: input.dateOfBirth,
+                  bio: input.biography,
+                  selectedUniversity: input.selectedUniversity,
+                  topIndustries: input.topIndustries,
+                }
+              },
+            },
+          },
+          include: {
+            profile: true,
+          }
+        })
+      } catch (error) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: '"{input.id}" not found in database',
+        });
+      }
+    }),
 });
