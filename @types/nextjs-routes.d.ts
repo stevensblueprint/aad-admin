694f44f55491a255c8653543f600aed6971fe74f
miguel-merlin <mmerlin@stevens.edu>
2024-04-02 09:45:15 -0400

    feat: Added types file

--- @types/nextjs-routes.d.ts
+++ @types/nextjs-routes.d.ts
@@ -11,12 +11,12 @@ declare module "nextjs-routes" {
   } from "next";
 
   export type Route =
-    | StaticRoute<"/Directory">
     | StaticRoute<"/admin/collections">
     | StaticRoute<"/admin">
     | DynamicRoute<"/api/auth/[...nextauth]", { "nextauth": string[] }>
     | StaticRoute<"/api/panel">
     | DynamicRoute<"/api/trpc/[trpc]", { "trpc": string }>
+    | StaticRoute<"/directory">
     | DynamicRoute<"/form/[id]", { "id": string }>
     | StaticRoute<"/">
     | DynamicRoute<"/profiles/[id]", { "id": string }>
@@ -43,8 +43,7 @@ declare module "nextjs-routes" {
     { pathname: P }
   >["query"];
 
-  export type Locale = 
-    | "en";
+  export type Locale = undefined;
 
   /**
    * A typesafe utility function for generating paths in your application.
@@ -63,11 +62,9 @@ declare module "nextjs-routes" {
   > = Omit<NextGetServerSidePropsContext, 'params' | 'query' | 'defaultLocale' | 'locale' | 'locales'> & {
     params: Extract<Route, { pathname: Pathname }>["query"];
     query: Query;
-    defaultLocale: "en";
-    locale: Locale;
-    locales: [
-          "en"
-        ];
+    defaultLocale?: undefined;
+    locale?: Locale;
+    locales?: undefined;
   };
 
   /**
@@ -101,7 +98,7 @@ declare module "next/link" {
     extends Omit<NextLinkProps, "href" | "locale">,
       AnchorHTMLAttributes<HTMLAnchorElement> {
     href: Route | StaticRoute | Omit<Route, "pathname">
-    locale?: Locale | false;
+    locale?: false;
   }
 
   type LinkReactElement = DetailedReactHTMLElement<
@@ -130,7 +127,7 @@ declare module "next/router" {
   type StaticRoute = Exclude<Route, { query: any }>["pathname"];
 
   interface TransitionOptions extends Omit<NextTransitionOptions, "locale"> {
-    locale?: Locale | false;
+    locale?: false;
   }
 
   type PathnameAndQuery<Pathname> = Required<
@@ -160,12 +157,10 @@ declare module "next/router" {
         | "replace"
         | "route"
       > & {
-        defaultLocale: "en";
+        defaultLocale?: undefined;
         domainLocales?: undefined;
-        locale: Locale;
-        locales: [
-          "en"
-        ];
+        locale?: Locale;
+        locales?: undefined;
         push(
           url: Route | StaticRoute | Omit<Route, "pathname">,
           as?: string,
