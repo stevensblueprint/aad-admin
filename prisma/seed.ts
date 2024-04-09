b68bcd6d882c43b633ef9e7f4b24a804fcdc2213
Phillip Anerine <paner225@gmail.com>
2024-04-09 13:05:52 -0400

    feat: mock data (#168)
    * refactor extracting create user and role functions
    
    * add populate script
    
    * init populate
    
    * fix lint/format issue
    
    * updated pnpm lock
    
    * refactor populate to schema and object parameter

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,113 +1,53 @@
-import { PrismaClient, UserRole } from "@prisma/client";
-import { Picsum } from "picsum-photos";
-
+import { PrismaClient } from "@prisma/client";
+import { createRole, createUser } from "./createData";
 const prisma = new PrismaClient();
-
-const createRole = async (prisma: PrismaClient, roleName: string) => {
-  const role = await prisma.userRole.upsert({
-    where: {
-      roleName,
-    },
-    update: {},
-    create: {
-      roleName,
-    },
-  });
-  return role;
-};
-
-const createUser = async (
-  prisma: PrismaClient,
-  id: string,
-  email: string,
-  name: string,
-  dateOfBirth: string,
-  preferredName: string,
-  phoneNumber: string,
-  bio: string,
-  roleName: string,
-  university: string,
-  industries: string[],
-) => {
-  const user = await prisma.user.upsert({
-    where: {
-      email,
-    },
-    update: {},
-    create: {
-      id,
-      name,
-      email,
-      image: Picsum.url({ height: 128, cache: false }),
-      role: {
-        connectOrCreate: {
-          create: {
-            roleName: roleName,
-          },
-          where: {
-            roleName: roleName,
-          },
-        },
-      },
-      profile: {
-        create: {
-          bio,
-          phoneNumber,
-          dateOfBirth,
-          preferredName,
-          university,
-          industries,
-        },
-      },
-    },
-  });
-  return user;
-};
 
 const main = async () => {
   const adminRole = await createRole(prisma, "admin");
   const mentorRole = await createRole(prisma, "mentor");
   const menteeRole = await createRole(prisma, "mentee");
-  const admin = await createUser(
-    prisma,
-    "admin0",
-    "admin0@email.com",
-    "First Admin",
-    "2000-01-02",
-    "Your Overlord",
-    "+1 (000)-000-0000",
-    "I am your overlord. Bow down to me!",
-    "ADMIN",
-    "Stevens Institute of Technology",
-    ["Education", "Human Resources", "Retail"],
-  );
-  const mentor = await createUser(
-    prisma,
-    "mentor0",
-    "mentor0@email.com",
-    "First Mentor",
-    "2005-05-20",
-    "Saving Grace",
-    "+1 (111)-111-1111",
-    "I am your saving grace. I will help you!",
-    "MENTOR",
-    "Stevens Institute of Technology",
-    ["Computer Science", "Chemical Engineering", "Information Technology"],
-  );
-  const mentee = await createUser(
-    prisma,
-    "mentee0",
-    "mentee0@email.com",
-    "First Mentee",
-    "2004-10-11",
-    "Near Helpless",
-    "+1 (222)-222-2222",
-    "I really need help!",
-    "MENTEE",
-    "Stevens Institute of Technology",
-    ["Information Technology", "Economics", "Accounting"],
-  );
-  console.log({ admin, mentor, mentee });
+  const admin = await createUser(prisma, {
+    id: "admin0",
+    email: "admin0@email.com",
+    name: "First Admin",
+    dateOfBirth: "2000-01-02",
+    preferredName: "First Admin",
+    phoneNumber: "+1 (000)-000-0000",
+    bio: "Your Overlord",
+    roleName: "ADMIN",
+    university: "Stevens Institute of Technology",
+    industries: ["Education", "Human Resources", "Retail"],
+  });
+
+  const mentor = await createUser(prisma, {
+    id: "mentor0",
+    email: "mentor0@email.com",
+    name: "First Mentor",
+    preferredName: "First Mentor",
+    dateOfBirth: "2005-05-20",
+    phoneNumber: "+1 (111)-111-1111",
+    bio: "Saving Grace",
+    roleName: "MENTOR",
+    university: "Stevens Institute of Technology",
+    industries: [
+      "Computer Science",
+      "Chemical Engineering",
+      "Information Technology",
+    ],
+  });
+
+  const mentee = await createUser(prisma, {
+    id: "mentee0",
+    email: "mentee0@email.com",
+    name: "First Mentee",
+    dateOfBirth: "2004-10-11",
+    preferredName: "First Mentee",
+    phoneNumber: "+1 (222)-222-2222",
+    bio: "Near Helpless",
+    roleName: "MENTEE",
+    university: "Stevens Institute of Technology",
+    industries: ["Information Technology", "Economics", "Accounting"],
+  });
 };
 
 main()
