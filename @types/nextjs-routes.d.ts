26a52d14bd872914b4b66d10e73097883efcfcfe
Miguel Merlin <106508795+miguel-merlin@users.noreply.github.com>
2024-04-09 18:19:36 -0400

    Feature/search sorting directory (#166)
    * feat: Added directory layout to Directory page
    
    * feat: Removed Directory layout from index.tsx
    
    * dependency: Added x-data-grid dependency
    
    * feat: Added DataGrid
    
    * fix: Fix layout padding
    
    * fix: Fixed format errors
    
    * feat: Added types file
    
    * feat: Added types
    
    * feat: Added types
    
    ---------
    
    Co-authored-by: Richard Kirk <92767187+ulrokx@users.noreply.github.com>

--- @types/nextjs-routes.d.ts
+++ @types/nextjs-routes.d.ts
@@ -45,8 +45,7 @@ declare module "nextjs-routes" {
     { pathname: P }
   >["query"];
 
-  export type Locale = 
-    | "en";
+  export type Locale = undefined;
 
   /**
    * A typesafe utility function for generating paths in your application.
@@ -65,11 +64,9 @@ declare module "nextjs-routes" {
   > = Omit<NextGetServerSidePropsContext, 'params' | 'query' | 'defaultLocale' | 'locale' | 'locales'> & {
     params: Extract<Route, { pathname: Pathname }>["query"];
     query: Query;
-    defaultLocale: "en";
-    locale: Locale;
-    locales: [
-          "en"
-        ];
+    defaultLocale?: undefined;
+    locale?: Locale;
+    locales?: undefined;
   };
 
   /**
@@ -103,7 +100,7 @@ declare module "next/link" {
     extends Omit<NextLinkProps, "href" | "locale">,
       AnchorHTMLAttributes<HTMLAnchorElement> {
     href: Route | StaticRoute | Omit<Route, "pathname">
-    locale?: Locale | false;
+    locale?: false;
   }
 
   type LinkReactElement = DetailedReactHTMLElement<
@@ -132,7 +129,7 @@ declare module "next/router" {
   type StaticRoute = Exclude<Route, { query: any }>["pathname"];
 
   interface TransitionOptions extends Omit<NextTransitionOptions, "locale"> {
-    locale?: Locale | false;
+    locale?: false;
   }
 
   type PathnameAndQuery<Pathname> = Required<
@@ -162,12 +159,10 @@ declare module "next/router" {
         | "replace"
         | "route"
       > & {
-        defaultLocale: "en";
+        defaultLocale?: undefined;
         domainLocales?: undefined;
-        locale: Locale;
-        locales: [
-          "en"
-        ];
+        locale?: Locale;
+        locales?: undefined;
         push(
           url: Route | StaticRoute | Omit<Route, "pathname">,
           as?: string,
