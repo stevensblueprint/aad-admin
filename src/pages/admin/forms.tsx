71bca834b1549a33a93d3250ed6af23bf45181d7
Caposto <christiand.apostol@outlook.com>
2024-04-11 03:37:40 -0400

    started forms page

--- src/pages/admin/forms.tsx
+++ src/pages/admin/forms.tsx
@@ -3,68 +3,9 @@ import AdminLayout from "../../components/layouts/AdminLayout";
 
 const Forms = () => {
   const utils = api.useUtils();
-  const [formName, setFormName] = useState<string>();
-  const [formSchema, setFormSchema] = useState<object>();
-  const [uiSchema, setUiSchema] = useState<UISchemaElement>({
-    type: "VerticalLayout",
-  });
   const { data, error, isLoading } = api.form.getForms.useQuery({
     includeSchemas: true,
   });
-
-  const updateForm = api.form.updateForm.useMutation();
-
-  /** TODO: Form Creation Interface */
-  // const { mutateAsync } = api.form.createForm.useMutation({
-  //   onSuccess: async () => {
-  //     await utils.form.getForms.invalidate();
-  //   },
-  // });
-
-  // const onSubmit: SubmitHandler<CreateCollectionData> = async (data) => {
-  //   await mutateAsync({
-  //     ...data,
-  //     roles: data.roles.map((role) => role.toUpperCase()),
-  //   });
-  // };
-
-  const handleRowClick = (
-    formName: string,
-    clickedFormSchema: object,
-    clickedUiSchema: UISchemaElement,
-  ) => {
-    setFormName(formName);
-    setFormSchema(clickedFormSchema);
-    setUiSchema(clickedUiSchema);
-  };
-
-  // TODO: Changes should be cached, and admin should hit a submit button to confirm
-  // Otherwise, multiple DB updates will start as opposed to one large one
-  // FIXME: updates are being written to database, but changes are not reflecting in the User Interface.
-  const handleEdit = async (
-    formName: string,
-    updatedFormSchema: string,
-    updatedUiSchema: string,
-  ) => {
-    await updateForm.mutateAsync({
-      id: formName,
-      newFormSchema: updatedFormSchema,
-      newUiSchema: updatedUiSchema,
-    });
-  };
-
-  // Extract the JSON and check the proper type from database row
-  const parseJSON = <T,>(jsonString: string, defaultValue: T): T => {
-    try {
-      return JSON.parse(jsonString) as T;
-    } catch (error) {
-      console.error("Error parsing JSON", error);
-      return defaultValue;
-    }
-  };
-
-  if (isLoading) return <DefaultLoadingPage />;
-  if (error) return <ErrorPage errorMessage={error.message} />;
 
   return (
     <Container className="mt-4">
@@ -79,55 +20,21 @@ const Forms = () => {
           </TableHead>
           <TableBody>
             {data?.map((form) => (
-              <TableRow
-                key={form.name}
-                onClick={() =>
-                  handleRowClick(
-                    form.name,
-                    parseJSON<object>(form.formSchema, {}),
-                    parseJSON<UISchemaElement>(form.uiSchema, {
-                      type: "VerticalLayout",
-                    }),
-                  )
-                }
-              >
+              <TableRow key={form.name}>
                 <TableCell>{form.name}</TableCell>
-                <TableCell>
-                  <JsonEditor
-                    data={() => parseJSON<object>(form.formSchema, {})}
-                    onUpdate={async (data) => {
-                      await handleEdit(
-                        formName,
-                        JSON.stringify(data.newData),
-                        JSON.stringify(uiSchema),
-                      );
-                    }}
-                  />
-                </TableCell>
-                <TableCell>
-                  <JsonEditor
-                    data={() =>
-                      parseJSON<UISchemaElement>(form.uiSchema, {
-                        type: "VerticalLayout",
-                      })
-                    }
-                    onUpdate={async (data) => {
-                      await handleEdit(
-                        formName,
-                        JSON.stringify(form.formSchema),
-                        JSON.stringify(data.newData),
-                      );
-                    }}
-                  />
-                </TableCell>
+                <TableCell>{form.formSchema}</TableCell>
+                <TableCell>{form.uiSchema}</TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-      <JsonForm schema={formSchema} uischema={uiSchema} initialData={{}} />
     </Container>
   );
+};
+
+Forms.getLayout = function getLayout(page: ReactElement) {
+  return <AdminLayout>{page}</AdminLayout>;
 };
 
 Forms.getLayout = function getLayout(page: ReactElement) {
