ce077b3e534b0abfd5c0d6d9ac3796680ac16a67
Richard Kirk <richard.kirk790@gmail.com>
2024-05-12 15:26:26 -0400

    Fix instrumentation.ts crash

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,11 +1,13 @@
 import { db } from "./server/db";
 
 export const register = async () => {
-  try {
-    await db.$connect();
-    console.log("Database connection successful!");
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    try {
+      await db.$connect();
+      console.log("Database connection successful!");
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
+    }
   }
 };
