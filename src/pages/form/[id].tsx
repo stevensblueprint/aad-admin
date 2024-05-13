30cc1261ac5da3202371c3210d307b530a2e7db7
Phillip Anerine <paner225@gmail.com>
2024-05-13 00:38:41 -0400

    Stylize form page (#195)
    * Style form, adding resubmit logic
    
    * Fix submit logic to prevent duplicate submissions
    
    Upsert only works with primary id, so easier to just explicity query
    then update or create.
    
    ---------
    
    Co-authored-by: Richard Kirk <richard.kirk790@gmail.com>

--- src/pages/form/[id].tsx
+++ src/pages/form/[id].tsx
@@ -1,7 +1,8 @@
-import { Container, Typography } from "@mui/material";
+import { Alert, Card, CardContent, Container, Typography } from "@mui/material";
 import { useSession } from "next-auth/react";
+import { useParams } from "next/navigation";
 import { useRouter } from "next/router";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { type JSONObject } from "superjson/dist/types";
 import ErrorPage from "../../components/error/error";
 import JsonForm from "../../components/forms/JsonForm";
@@ -14,39 +15,98 @@ import { api } from "../../utils/api";
  */
 const Form = () => {
   useSession({ required: true });
+  const [submitted, setSubmitted] = useState(false);
+  const [countdown, setCountdown] = useState(4);
   const router = useRouter();
-  const { data, error, isLoading } = api.collection.getCollectionById.useQuery({
-    id: router.query.id as string,
-  });
-  const { mutate: submit } = api.form.submitForm.useMutation();
+  const { id } = useParams();
+  const { data, error, status } =
+    api.collection.getMyCollectionSubmission.useQuery(
+      {
+        collectionId: id as string,
+      },
+      { retry: false, refetchOnWindowFocus: false },
+    );
+
+  const {
+    mutateAsync: submit,
+    status: submitStatus,
+    error: submitError,
+  } = api.form.submitForm.useMutation();
   const { formSchema, uiSchema } = useMemo(() => {
     if (!data) return {};
     return {
-      formSchema: JSON.parse(data.form.formSchema),
-      uiSchema: JSON.parse(data.form.uiSchema),
+      formSchema: JSON.parse(data.collection.form.formSchema),
+      uiSchema: JSON.parse(data.collection.form.uiSchema),
     };
   }, [data]);
-  if (isLoading) return <DefaultLoadingPage />;
-  if (error) return <ErrorPage errorMessage={error.message} />;
-  const onSubmit = (data: JSONObject) => {
-    submit({ collectionId: router.query.id as string, data });
+
+  const onSubmit = async (data: JSONObject) => {
+    await submit({ collectionId: router.query.id as string, data });
+    setSubmitted(true);
+    const interval = setInterval(() => {
+      setCountdown((prev) => {
+        if (prev === 1) {
+          clearInterval(interval);
+          void router.push("/");
+        }
+        return prev - 1;
+      });
+    }, 1000);
   };
+
+  let body: JSX.Element = <></>;
+
+  if (status === "loading" || submitStatus === "loading") {
+    return <DefaultLoadingPage />;
+  } else if (status === "error") {
+    return <ErrorPage errorMessage={error.message} />;
+  } else if (submitStatus === "error") {
+    return <ErrorPage errorMessage={submitError.message} />;
+  } else if (submitted) {
+    body = (
+      <Card className="mx-3 p-4 text-center text-black">
+        <h1 className="text-3xl">Thank you!</h1>
+        <p className="text-xl">
+          Your submission has been recieved! Redirecting you home in {countdown}{" "}
+          seconds...
+        </p>
+      </Card>
+    );
+  } else if (status === "success") {
+    const alreadySubmitted = !!data.submission?.data;
+    body = (
+      <Container className="my-4 flex w-full flex-col gap-4 items-center">
+        <Typography variant="h4" className="font-bold text-white">
+          {data.collection.name}
+        </Typography>
+        <Card>
+          <CardContent className="p-8">
+            <Typography variant="body1">
+              {data.collection.instructions}
+            </Typography>
+            {alreadySubmitted && (
+              <Alert severity="info" className="mt-4">
+                You have already submitted this form, but feel free to edit your
+                responses and submit the form again. Your new submission will
+                replace your old submission.
+              </Alert>
+            )}
+          </CardContent>
+        </Card>
+        <JsonForm
+          schema={formSchema}
+          uischema={uiSchema}
+          initialData={data.submission?.data ?? {}}
+          onSubmit={(data) => void onSubmit(data)}
+        />
+      </Container>
+    );
+  }
+
   return (
-    <Container className="flex flex-col p-4">
-      <Typography variant="h4" className="mb-4">
-        {data.name}
-      </Typography>
-      <Typography variant="body1" className="mb-4">
-        {data.instructions}
-      </Typography>
-      <JsonForm
-        schema={formSchema}
-        uischema={uiSchema}
-        initialData={{}}
-        onSubmit={onSubmit}
-      />
+    <Container className="flex min-h-screen min-w-full flex-col items-center bg-gradient-to-b from-midnight-sky to-aero p-8">
+      {body}
     </Container>
   );
 };
-
 export default Form;
